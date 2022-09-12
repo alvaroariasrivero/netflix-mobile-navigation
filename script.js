9b573314d7ff70f3d1215c11e6a11f96a50f99eb
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-09-12 22:53:58 +0200

    dropdown list

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const open_btn = document.querySelector('.open-btn');
 const close_btn = document.querySelector('.close-btn');
 const nav = document.querySelectorAll('.nav');
+const docu = document.querySelector('.docu');
+const docuDropdown = document.querySelector('.docu-dropdown');
 
 open_btn.addEventListener('click', () => {
-    nav.forEach(nav_el => nav_el.classList.add('visible'))
+    nav.forEach(nav_el => nav_el.classList.add('visible'));
 });
 
 close_btn.addEventListener('click', () => {
-    nav.forEach(nav_el => nav_el.classList.remove('visible'))
+    nav.forEach(nav_el => nav_el.classList.remove('visible'));
+});
+
+docu.addEventListener('click', () => {
+    docuDropdown.classList.toggle('none');
 });
